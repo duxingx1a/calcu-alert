bc76f1ab9fcfbd0a54ff4ac95ddd4ed755e550fa
ydl <247866566@qq.com>
2026-07-25 12:08:27 +0800

    layout: 2x2 grid with force plot at (1,0) and prob spanning 2 rows

--- assets/js/demo-app.js
+++ assets/js/demo-app.js
@@ -1,6 +1,6 @@
-import { getExamples, getHealth, getMetadata, predict } from './api-client.js?v=250737';
-import { collapseAllSections, fillValues, readValues, renderForm, SAMPLES, validate } from './feature-schema.js?v=250737';
-import { clearResult, renderPrediction, toggleResultPanel } from './shap-view.js?v=250737';
+import { getExamples, getHealth, getMetadata, predict } from './api-client.js?v=250738';
+import { collapseAllSections, fillValues, readValues, renderForm, SAMPLES, validate } from './feature-schema.js?v=250738';
+import { clearResult, renderPrediction, toggleResultPanel } from './shap-view.js?v=250738';
 
 let metadata;
 let busy = false;
